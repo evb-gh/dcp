ca60b906fc10a2aca736fe94933bc78db4000c30
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-10-01 10:49:29 -0700

    enable question and numeric questions tests

--- Exercism/javascript/bob/bob.spec.js
+++ Exercism/javascript/bob/bob.spec.js
@@ -16,12 +16,12 @@ describe('Bob', () => {
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  xtest('asking a question', () => {
+  test('asking a question', () => {
     const result = hey('Does this cryogenic chamber make me look fat?');
     expect(result).toEqual('Sure.');
   });
 
-  xtest('asking a numeric question', () => {
+  test('asking a numeric question', () => {
     const result = hey('You are, what, like 15?');
     expect(result).toEqual('Sure.');
   });
